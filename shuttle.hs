805f429888094f8fefd3524f2f33cd41a91823ef
Tiziano Santoro <tzn@google.com>
2015-02-06 22:20:33 +0000

    better

--- shuttle.hs
+++ shuttle.hs
@@ -18,7 +18,6 @@ data Event = Event {
 data State = State {
   jog :: Int32,
   wheel :: Int32,
-  wheelDelta :: Int32,
   mode :: Int32
 } deriving (Eq, Show)
 
@@ -38,7 +37,11 @@ handleEvent Event{ etype = 1, code = 261, value = 1 } s = return s{ mode = 1 }
 handleEvent Event{ etype = 1, code = 262, value = 1 } s = return s{ mode = 2 }
 handleEvent Event{ etype = 1, code = 263, value = 1 } s = return s{ mode = 3 }
 handleEvent Event{ etype = 1, code = 264, value = 1 } s = return s{ mode = 4 }
-handleEvent Event{ etype = 2, code = 7, value = v } s = return s{ wheel = v }
+handleEvent Event{ etype = 2, code = 7, value = v } s = do
+  if v - (wheel s) > 0
+  then clickAction ButtonWheelDown
+  else clickAction ButtonWheelUp
+  return s{ wheel = v }
 handleEvent Event{ etype = 2, code = 8, value = v } s = return s{ jog = v }
 handleEvent _ s = return s
 
@@ -48,45 +51,22 @@ initialState :: State
 initialState = State {
   jog = 0,
   wheel = 0,
-  wheelDelta = 0,
   mode = 0
 }
 
-clickAction :: Button -> [String]
-clickAction b = ["click", show bb]
+clickAction :: Button -> IO ()
+clickAction b = xdotool ["click", show bb]
   where bb = case b of
-              ButtonLeft -> 1
-              ButtonMiddle -> 2
-              ButtonRight -> 3
-              ButtonWheelUp -> 4
-              ButtonWheelDown -> 5
-
-convertAction :: Action -> [String]
-convertAction ScrollUp = clickAction ButtonWheelUp
-convertAction ScrollDown = clickAction ButtonWheelDown
-convertAction Click = clickAction ButtonLeft
-
-performAction :: Action -> IO ()
-performAction a = let arg = convertAction a in
-  xdotool arg
-
-noAction :: IO ()
-noAction = return ()
+             ButtonLeft -> 1
+             ButtonMiddle -> 2
+             ButtonRight -> 3
+             ButtonWheelUp -> 4
+             ButtonWheelDown -> 5
 
 xdotool :: [String] -> IO ()
 xdotool arg = do
   createProcess $ proc "xdotool" arg
   return ()
-
-fromEvent :: Event -> Maybe (IO ())
-fromEvent e = case etype e of
-                1 -> if value e == 1
-                     then case code e of
-                            261 -> Nothing -- Just $ performAction ScrollUp
-                            262 -> Nothing -- Just $ performAction ScrollDown
-                            _ -> Nothing
-                     else Nothing
-                _ -> Nothing
 
 deserialize :: Get Event
 deserialize = do
@@ -107,9 +87,10 @@ mainLoop :: Handle -> State -> IO ()
 mainLoop h oldState = do
   buf <- B.hGet h 24
   let e = runGet deserialize buf
-  putStrLn $ show $ e
   newState <- handleEvent e oldState
+  putStrLn $ show $ e
   putStrLn $ show newState
+  putStrLn "----"
   mainLoop h newState
 
 main :: IO ()
